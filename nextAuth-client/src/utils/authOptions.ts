11606f3055ec0636de979132d27ea90c55cbc3ef
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-03 15:44:47 +0600

    Implementing github login functionalities

--- nextAuth-client/src/utils/authOptions.ts
+++ nextAuth-client/src/utils/authOptions.ts
@@ -1,6 +1,12 @@
+import GithubProvider from 'next-auth/providers/github';
 import { NextAuthOptions } from 'next-auth';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
-  providers: [],
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
 };
